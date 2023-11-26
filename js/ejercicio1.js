c1890a2ca1358af417475d492218615a045ddaae
Elias Juarez <juarezelias83@gmail.com>
2023-11-26 18:28:49 -0300

    se creo una funcion que comprueba si el numero ingresado es igual o no al numero magico, y se le pasa el evento click junto con la funcion al boton de enviar

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,19 +1,36 @@
 const btnComenzarJuego = document.querySelector(".btnComenzarJuego");
-const seccion = document.getElementById("seccion");
+const form = document.getElementById("form");
 let numAleatorio = 0;
 
 function comenzarJuego() {
+  btnComenzarJuego.className = "ocultarBoton";
   numAleatorio = Math.round(Math.random() * 50);
+  console.log(numAleatorio);
+
   const titulo = document.createElement("h2");
   titulo.innerHTML = "Adivine el número mágico";
-  seccion.appendChild(titulo);
+  form.appendChild(titulo);
+
   const input = document.createElement("input");
-  input.className = "form-control w-50 my-4";
-  seccion.appendChild(input);
+  console.log(input.value);
+  input.className = "form-control w-100 my-4";
+  form.appendChild(input);
+
   const btnEnviar = document.createElement("button");
-  btnEnviar.innerHTML = "Enviar"
-  btnEnviar.className = "btn btn-primary"
-  seccion.appendChild(btnEnviar);
+  btnEnviar.innerHTML = "Enviar";
+  btnEnviar.className = "btn btn-primary";
+  form.appendChild(btnEnviar);
+  const adivinarNumero = (e) => {
+    e.preventDefault();
+    if (input.value == numAleatorio) {
+      alert("¡Adivinaste el número mágico!");
+    } else if (input.value > numAleatorio) {
+      alert("El número ingresado es mayor al número mágico");
+    } else {
+      alert("El número ingresado es menor al número mágico");
+    }
+  };
+  btnEnviar.addEventListener("click", adivinarNumero);
 }
 
 btnComenzarJuego.addEventListener("click", comenzarJuego);
